9ca96e2555eb380c2fb5fd5285b1c2ea8f8c1c1b
gonzalo <gonzalo.trucco40@gmail.com>
2022-08-30 14:13:52 +0200

    create basic api

---
+++ Api/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRouter from "./routes/auth.js";
+import usersRouter from "./routes/users.js";
+import hotelsRouter from "./routes/hotels.js";
+import roomsRouter from "./routes/rooms.js";
+
+const app = express();
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongoDB");
+  } catch (err) {
+    throw err;
+  }
+};
+
+//middlewares
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/hotels", hotelsRouter);
+app.use("/api/rooms", roomsRouter);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("connected to backend!!!");
+});
